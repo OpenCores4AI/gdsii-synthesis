eba7cf68eaa409e6ea360f6c31398ed856c87e59
David Alejandro Trejo Pizzo <dtrejopizzo@gmail.com>
2026-02-20 15:29:30 -0300

    Add counter_2bit for GDSII synthesis

---
+++ projects/Alv0UUQu-1/counter_2bit.v
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity counter_2bit is
+    Port (
+        clk   : in  STD_LOGIC;
+        rst   : in  STD_LOGIC;
+        count : out STD_LOGIC_VECTOR (1 downto 0)
+    );
+end counter_2bit;
+
+architecture Behavioral of counter_2bit is
+    signal count_reg : STD_LOGIC_VECTOR (1 downto 0) := "00";
+begin
+    process(clk, rst)
+    begin
+        if rst = '1' then
+            count_reg <= "00";
+        elsif rising_edge(clk) then
+            count_reg <= count_reg + 1;
+        end if;
+    end process;
+    
+    count <= count_reg;
+end Behavioral;
