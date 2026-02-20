ebe6f3efa3fcaec7814d504201948956a3664b31
David Alejandro Trejo Pizzo <dtrejopizzo@gmail.com>
2026-02-20 13:50:15 -0300

    Add counter_2bit for GDSII synthesis

---
+++ projects/UuW_Ak3lhg/counter_2bit.v
@@ -0,0 +1,15 @@
+module counter_2bit (
+    input clk,
+    input reset,
+    output reg [1:0] count
+);
+
+always @(posedge clk or posedge reset) begin
+    if (reset) begin
+        count <= 2'b00;
+    end else begin
+        count <= count + 1;
+    end
+end
+
+endmodule
