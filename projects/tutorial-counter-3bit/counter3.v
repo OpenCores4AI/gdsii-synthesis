0ed3eaeecf949d41e598c11c6ef09f509d467f2f
David Alejandro Trejo Pizzo <dtrejopizzo@gmail.com>
2026-05-26 22:02:58 -0300

    tutorial(counter-3bit): refresh counter3.v

---
+++ projects/tutorial-counter-3bit/counter3.v
@@ -0,0 +1,13 @@
+module counter3 (
+  input  wire       clk,
+  input  wire       rst,
+  input  wire       en,
+  output reg  [2:0] q,
+  output wire       tc
+);
+  always @(posedge clk) begin
+    if (rst)     q <= 3'b000;
+    else if (en) q <= q + 3'b001;
+  end
+  assign tc = (q == 3'b111);
+endmodule
